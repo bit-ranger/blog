32659c2946215af58e610280cf0c51a2aae3efce
dubuyuye <sincerebravefight@gmail.com>
2015-07-09 22:09:53 +0000

    styles

--- static/js/script.js
+++ static/js/script.js
@@ -5,39 +5,50 @@ $(document).ready(function() {
 
     console.log("你不乖哦，彼此之间留点神秘感不好吗？");
 
+    generateContent();
     hljs.initHighlightingOnLoad();
     backToTop();
     categoryDisplay();
     duoshuoQueryFunction();
-    generateContent();
     share();
 });
 
-
+/**
+ * 侧边目录
+ */
+function generateContent() {
+    var toc = $("#markdown-toc").html();
+    if (typeof toc != "undefined") {
+        $(".content-navigation").addClass("col-sm-3").show();
+        $(".myArticle").addClass("col-sm-9");
+        $(".content-navigation .content-navigation-text").html("<ul>" + toc + "</ul>");
+    }
+}
 
 /**
  * 回到顶部
  */
 function backToTop() {
+    $("[data-toggle='tooltip']").tooltip();
+    var st = $(".page-scrollTop");
+    var $window = $(window);
     //滚页面才显示返回顶部
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 100) {
-            $(".page-scrollTop").fadeIn(500);
+    $window.scroll(function() {
+        if ($window.scrollTop() > 0) {
+            st.fadeIn(500);
         } else {
-            $(".page-scrollTop").fadeOut(500);
+            st.fadeOut(500);
         }
     });
+
     //点击回到顶部
-    $(".page-scrollTop").click(function() {
+    st.click(function() {
         $("body").animate({
             scrollTop: "0"
         }, 500);
     });
 
-    //初始化tip
-    $(function() {
-        $("[data-toggle='tooltip']").tooltip();
-    });
+
 }
 
 /**
@@ -47,14 +58,11 @@ function backToTop() {
  * @return {[type]} [description]
  */
 function categoryDisplay() {
-    /*only show All*/
     $('.post-list-body>div[post-cate!=All]').hide();
-    /*show category when click categories list*/
     $('.categories-list-item').click(function() {
         var cate = $(this).attr('cate'); //get category's name
-
-        $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
-        $('.post-list-body>div[post-cate=' + cate + ']').show(400);
+        $('.post-list-body>div[post-cate!=' + cate + ']').hide();
+        $('.post-list-body>div[post-cate=' + cate + ']').show(200);
     });
 }
 
@@ -71,18 +79,7 @@ function duoshuoQueryFunction(){
 	document.body.appendChild(ds);
 }
 
-/**
- * 侧边目录
- */
-function generateContent() {
 
-    if (typeof $("#markdown-toc").html() === "undefined") {
-        $(".content-navigation").hide();
-        $(".myArticle").removeClass("col-sm-9").addClass("col-sm-12");
-    } else {
-        $(".content-navigation .content-navigation-text").html("<ul>" + $("#markdown-toc").html() + "</ul>");
-    }
-}
 
 function share(){
     window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"share":{}};
