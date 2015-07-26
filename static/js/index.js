5da82e2060d584126baaa0637259ad0562fe2aa7
dubuyuye <sincerebravefight@gmail.com>
2015-07-26 21:16:04 +0000

    hash event

--- static/js/index.js
+++ static/js/index.js
@@ -21,11 +21,19 @@ function categoryDisplay() {
 }
 
 function selectCategory(){
+    var exclude = ["",undefined];
     var thisId = window.location.hash.substring(1);
-    if(thisId != "" && thisId != undefined){
+    var allow = true;
+    for(var i in exclude){
+        if(thisId == exclude[i]){
+            allow = false;
+            break;
+        }
+    }
+    if(allow){
         var cate = thisId;
-        $('section[post-cate!=' + cate + ']').hide(200);
-        $('section[post-cate=' + cate + ']').show(200);
+        $("section[post-cate!='" + cate + "']").hide(200);
+        $("section[post-cate='" + cate + "']").show(200);
     } else {
         $("section[post-cate='All']").show();
     }
