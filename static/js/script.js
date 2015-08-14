86fc883e03578dbb0c6c5262e20725f09db90c42
dubuyuye <sincerebravefight@gmail.com>
2015-08-14 15:19:24 +0800

    swiftype

--- static/js/script.js
+++ static/js/script.js
@@ -44,7 +44,7 @@ function search(){
         e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.appendChild(s);
     })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
 
-    _st('install','WyVgxJw2ypny5Feaxyrs','2.0.0');
+    _st('install','ncwR9a8S9CTx2q9SgsJm','2.0.0');
 }
 
 
