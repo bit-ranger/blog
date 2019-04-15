dff63855fddc13f4893e21bcfac98161eec13120
Weidong Sun <sunweidong90@gmail.com>
2019-04-15 15:24:05 -0500

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -43,12 +43,12 @@ function backToTop() {
 }
 
 function search(){
-    (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
-        (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
-        e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.appendChild(s);
-    })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
-
-    _st('install','{{site.swiftype_searchId}}','2.0.0');
+  (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+  (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+  e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+  })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+  
+  _st('install','{{site.swiftype_searchId}}','2.0.0');
 }
 
 
